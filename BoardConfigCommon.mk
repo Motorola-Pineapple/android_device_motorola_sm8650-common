c3c4f40f2cc15ad538fbfbc43295d157d595157d
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-29 16:28:58 +0300

    sm8650-common: Import config.fs from LA.VENDOR.14.3.0.r1-22400-lanai.QSSI15.0
    Change-Id: If42761a1a7c8d022425d0e76ed705d18003e4bd4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,9 @@ TARGET_CPU_VARIANT_RUNTIME := kryo385
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := pineapple
